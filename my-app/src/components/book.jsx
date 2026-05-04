e35d1232b8b13935869b2e5932c29b7e49432b73
Juan <juandiego.leonm3533@comunidadunir.net>
2026-05-04 18:57:08 -0500

    Added tailwindsCSS and DaisyUI

--- my-app/src/components/book.jsx
+++ my-app/src/components/book.jsx
@@ -1,11 +1,21 @@
 import portada from '../assets/book_cards/978958421602.webp'
 function Book(){
     return(
-        <div className="book">
-            <img src={portada} width="300" height="300"/>
-            <h3>Libro</h3>
-            <p>Autor</p>
-            <p>Año</p>
+        <div className="card card-side bg-base-50 w-64 shadow-sm border-2">
+            <figure>
+                <img className="align-middle"
+                    src= {portada}
+                    alt="Book" />
+            </figure>
+            <div className="card-body text-left">
+                <h2 className="card-title">Nuevo libro disponible</h2>
+                <p>El perfume.</p>
+                <p>Patrcik Süskind</p>
+                <p>1985</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Agregar al carrito</button>
+                </div>
+            </div>
         </div>
     )
 }
